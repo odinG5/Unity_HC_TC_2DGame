7df6a625f832aecaa347be50161b7a4fb131d14b
odinG5 <57181291+odinG5@users.noreply.github.com>
2019-11-20 21:41:44 +0800

    刪除 API 使用與測試

--- Unity Project 2D/Assets/Scnipts/GameManager.cs
+++ Unity Project 2D/Assets/Scnipts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
     public int score;
     [Header("最佳分數")]
     public int scoreHeight;
+    [Header("水管群組")]
+    public GameObject pipe; //GameOject 可以存場景上的物件也可以存專案內的預製物
 
     /// <summary>
     /// 加分
@@ -30,5 +32,32 @@ public class GameManager : MonoBehaviour
     private void GaneOver()
     {
 
+    }
+
+    /// <summary>
+    /// 生成水管的方式
+    /// </summary>
+    private void SpawnPipe()
+    {
+        // 三維向量 = new 三維向量(x, y, z);
+        //Vector3 p = new Vector3(6, Random.Range(-0.7f, 1.3f), 0);
+
+        float r = Random.Range(-0.7f, 1.3f);
+        Vector3 p = new Vector3(6, r, 0);
+
+        
+        // Quaternion.identity 零角度
+
+        // 實例化 - 生成(物件)
+        Instantiate(pipe, p, Quaternion.identity);
+    }
+
+    private void Start()
+    {
+        //SpawnPipe();
+        // 延遲調用("方法名稱"，延遲時間)
+        // Invoke("SpawnPipe", 1.5f);
+        // 延遲重複調用("方法名稱" 延遲時間，重複頻率);
+        InvokeRepeating("SpawnPipe", 0, 1.8f);
     }
 }
---
+++ Unity Project 2D/Assets/Scnipts/Pipe.cs
@@ -0,0 +1,9 @@
+﻿//：繼承 規則，可以拿有繼承規則的成員
+public class Pipe : Floor
+{
+    private void Start()
+    {
+        // 刪除(五遊戲物件，延遲2.5 秒);
+        Destroy(gameObject, 5f);
+    }
+}
