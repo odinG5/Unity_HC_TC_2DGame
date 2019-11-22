1f8ebc1e0bfb71acdf1171b1009fda6fa0f6cc66
odinG5 <57181291+odinG5@users.noreply.github.com>
2019-11-22 21:44:30 +0800

    完成判斷式練習

---
+++ Unity Project 2D/Assets/Scnipts/LearnOperator.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int a = 10, b = 3;
+
+    public int c = 20, d = 7;
+
+    private void Start()
+    {
+        #region 數學運算子
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b);
+
+        // = 指定符號，右邊先運算
+        a = a + 1;
+        print(a);
+
+        print(a++);  // 先輸出 a 在執行遞增
+        print(++a);  // 先執行遞增在輸出 a
+
+        a = a + 10;
+        a += 10;         // += -= *= /= %=
+        print(a);
+        #endregion
+
+        #region 比較運算子
+        print(c > d);   // true
+        print(c < d);   // false
+        print(c >= d);  // true
+        print(c <= d);  // false
+        print(c == d);  // false  等於
+        print(c != d);  // true  不等於
+        #endregion
+
+        #region 邏輯運算子
+        // && 並且 shift + 7
+        // 只要有一個 false 結果就是 false
+        print(true && true);
+        print(true && false);
+        print(false && true);
+        print(false && false);
+
+        // || 或者 shift + 鎮
+        // 只要有一個 true 結果就是 true
+        print(true || true);
+        print(true || false);
+        print(false || true);
+        print(false || false);
+        #endregion
+    }
+}
---
+++ Unity Project 2D/Assets/Scnipts/Learnlt.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Learnlt : MonoBehaviour
+{
+    public bool open;
+
+    public int score;
+
+    public int combo;
+
+    private void Start()
+    {
+        // 判斷式 if
+        // if (布林值) { 當不林值為 true 才執行 }
+
+        if (true)
+        {
+            print("嗨~");
+        }
+
+        // 當布林值為 true 才執行 if {}
+        if (open)
+        {
+            print("開門~");
+        }
+        // 當布林值為 false 才執行 else{}
+        else
+        {
+            print("關門~");
+        }
+
+        if (score >= 60)
+        {
+            print("及格~");
+        }
+        else if (score >= 40)
+        {
+            print("補考");
+        }
+        else
+        {
+            print("你被當了！");
+        }
+    }   
+
+    private void Update()
+    {
+        if (combo >= 150)
+        {
+            print("攻擊 * 2");
+        }
+        else if (combo >= 100)
+        {
+            print("攻擊 * 5");
+        }
+        else if (combo >= 50)
+        {
+            print("攻擊 * 10");
+        }
+    }
+}
