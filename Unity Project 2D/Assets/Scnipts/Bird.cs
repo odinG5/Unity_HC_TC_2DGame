20a9b568ddf71796c005df4316ba6f15fd7e9d57
odinG5 <57181291+odinG5@users.noreply.github.com>
2019-11-27 22:04:40 +0800

    完成設定觸發

--- Unity Project 2D/Assets/Scnipts/Bird.cs
+++ Unity Project 2D/Assets/Scnipts/Bird.cs
@@ -2,32 +2,70 @@
 
 public class Bird : MonoBehaviour
 {
-   [Header("跳躍高度"),Range(50,1500)]
+    [Header("跳躍高度"),Range(50,1500)]
     public int jumpHeight = 100;
+    [Header("旋轉角度")]
+    public float angle = 5;
     [Header("是否死亡"), Tooltip("判斷角色是否死亡")]
     public bool isDead;
+
+    public GameObject goScore, goGM;
+    public Rigidbody2D r2d;      // 2D 剛體
+
+    private void Update()
+    {
+        jump();
+    }
+   
+    // 碰種開始事件：物件碰撞開始執行一次 (紀錄碰撞物件資訊) - 針對有勾選 isTrigger
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 碰撞.遊戲物件.名稱
+        // print(collision.gameObject.name);
+
+        Dead();
+    }
+    // 觸發開始物件：物件碰撞開始執行一次 (紀錄碰撞物件資訊)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
 
     /// <summary>
     /// 小雞跳躍功能
     /// </summary>
     private void jump()
     {
+        if (isDead) return;
 
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            goScore.SetActive(true);
+            goGM.SetActive(true);
+
+            r2d.Sleep();                                    // 2D 剛體.睡著() 重設所有物理資料
+            r2d.gravityScale = 1;                           // 2D 剛體.地心引力 = 1
+            r2d.AddForce(new Vector2(0, jumpHeight));       // 2D 剛體.推力(二為向量)
+        
+        }
+
+       r2d.SetRotation(angle * r2d.velocity.y);                                // 2D 剛體.旋轉(角度)
+       //print(r2d.velocity);
     }
 
     /// <summary>
-    /// 小雞死亡功能
-    /// </summary>
+   /// 小雞死亡功能
+   /// </summary>
     private void Dead()
     {
 
     }
 
     /// <summary>
-    /// 通過水管
-    /// </summary>
+   /// 通過水管
+   /// </summary>
     private void PassPipe()
     {
-
+        
     }
 }
--- Unity Project 2D/Assets/Scnipts/Lod.cs
+++ Unity Project 2D/Assets/Scnipts/Lod.cs
@@ -4,8 +4,9 @@ public class Lod : MonoBehaviour
 {
     public int speed = 0;
 
-    
-    
+    // 非靜態需要定義欄位，並且在屬性面板上套用動應物件
+    public Transform bird;
+    public Transform groumd;
 
     private void Start()
     {
@@ -20,8 +21,16 @@ public class Lod : MonoBehaviour
         // 靜態方式 static method
         Debug.Log(Random.Range(1, 10));
 
-        
+        // 非靜態屬性用法 properties
+        Debug.Log(bird.position);
+        Debug.Log(groumd.position);
     }
 
-    
+    // 更新事件：偵測按鍵
+    private void Update()
+    {
+        print(Input.GetKeyDown("mouse 0"));               // 字串取得按鍵
+        print(Input.GetKeyDown(KeyCode.Mouse1));          // 列舉取得按鍵
+    }
+
 }
